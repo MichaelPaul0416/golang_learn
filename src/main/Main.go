643618fb1a6cd72e58288f155ef79f43b99aeca1
MichaelPaul0416 <18357903752m0@sina.cn>
2019-07-22 22:56:27 +0800

    golang的sort接口学习，代码提交

--- src/main/Main.go
+++ src/main/Main.go
@@ -2,55 +2,37 @@ package main
 
 import (
 	"../chapter7"
+	"sort"
 	"fmt"
-	"flag"
-	"time"
-	"io"
-	"os"
-	"bytes"
 )
 
-func main() {
-	//空的接口类型可以赋值给任意的类型
-	var any chapter7.Empty
-	any = true
-	fmt.Printf("empty -> boolean:%t\n", any)
-	any = 1
-	fmt.Printf("empty -> int:%d\n", any)
+func main(){
+	s1 := make([]string,10)
+	s1 = append(s1,"b")
+	s1 = append(s1,"c")
+	chapter7.SortSlice(s1)
 
-	//使用.Value解析
-	//flag.Duration：创建一个命令行参数，参数名是period，参数的默认值是1s，也就是第二个参数指定值，参数解释是第三个参数值
-	var period = flag.Duration("period", 1*time.Second, "sleep period") //返回的是一个指针类型
-	flag.Parse()
-	fmt.Printf("Sleeping for %v...\n", *period)
-	time.Sleep(*period)
-	fmt.Println("done")
+	s1 = append(s1,"a")
+	sort.Strings(s1)
+	fmt.Printf("go api sort:%v\n",s1)
 
-	//返回一个指针类型
-	var c chapter7.Celsius
-	c.Init(20.0)
-	var temp = chapter7.ChangeTemperature("temp", c, "the temperature")
-	flag.Parse()
-	fmt.Printf("%v\n",*temp)
+	chapter7.SortTracksByArtist()
 
-	//接口值=动态类型[java多态中的实际类型]+动态值[实际类型的实例对象]
-	//输出动态类型
-	var w io.Writer
-	w = os.Stdout
-	fmt.Printf("real type:%T\n",w)
-	w = new(bytes.Buffer)
-	fmt.Printf("real type:%T\n",w)
+	chapter7.SortWithMulti()
 
-	//debug := true
-	debug := false//使用false话，那么仅仅只是申明了buf的类型，没有关联实例对象[java中的话，一般会报空指针]
-	//下面这行语句，对于接口值的赋值，仅仅是给了动态类型是一个byte.Buffer的指针，但是对于动态值，没有进行赋值
-	var buf *bytes.Buffer
-	fmt.Printf("pointer null:%t\n",buf == nil)//输出true
-	if debug{
-		buf = new(bytes.Buffer)//赋值了动态类型和动态值，所以接口值是不为空的
-	}
+	var ary = []int{2,3,1,5,4,1}
+	fmt.Printf("is sorted:%t\n",sort.IntsAreSorted(ary))//sort.IntsAreSorted不会去调用sort#Swap方法，只调用其他两个
+	sort.Ints(ary)
+	fmt.Printf("is sorted:%t\n",sort.IntsAreSorted(ary))
 
-	buf.WriteString("hello")
-	fmt.Printf("%s\n",buf.String())
-
+	is := sort.IntSlice(ary)//封装，实现sort接口的方法
+	fmt.Printf("ary:%v\n",is)
+	//sort.Reverse只接受sort接口的实现，所以slice不能直接传入，需要使用sort提供的IntSlice封装一下
+	sort.Sort(sort.Reverse(is))
+	fmt.Printf("reverse ary:%v\n",is)
+	//因为这个方法最终是去调用对应类的Less方法，而reverse只是将原先传入的i,j顺序掉换成j,i
+	//原先的Less返回的是排好序之后s[i] < s[j] = true
+	//现在i->j j->i, 所以s[j] < s[i] = false
+	//所以此时IsSorted返回false
+	fmt.Printf("is sorted:%t\n",sort.IsSorted(is))
 }
