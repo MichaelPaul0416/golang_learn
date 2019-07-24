7f64dd6f30a4c7e2a219afbe08579bf21b4fefc2
MichaelPaul0416 <18357903752m0@sina.cn>
2019-07-24 22:53:58 +0800

    使用断言进行错误类型的判断，然后获取错误的原始信息

--- src/chapter7/Assert.go
+++ src/chapter7/Assert.go
@@ -6,12 +6,32 @@ import (
 	"fmt"
 )
 
-func AssertTip(){
+func AssertTip(c bool) {
 	var w io.Writer
 	w = os.Stdout
 	//w.Close()//此时w声明的类型为io.Writer,不具备Close方法
 
-	f,ok := w.(*os.File)//断言w为*os.File类型-->true
-	fmt.Printf("assert io.Writer --> *os.File:%t\n",ok)
-	f.Close()//返回的f是断言的类型，也就是*os.File,具有Close方法
+	f, ok := w.(*os.File) //断言w为*os.File类型-->true
+	fmt.Printf("assert io.Writer --> *os.File:%t\n", ok)
+	if c {
+		f.Close() //返回的f是断言的类型，也就是*os.File,具有Close方法
+	}
+}
+
+func ShowErrorType() {
+	_, err := os.Open("/hello/world")
+	fmt.Printf("%v\n", err)  //输出错误信息
+	fmt.Printf("%#v\n", err) //输出原始信息
+}
+
+//使用断言来返回错误
+func AssertWithError() {
+	_,err := os.Open("/no/such/file")
+
+	if pe,ok := err.(*os.PathError);ok{
+		//如果类型断言成功，那么err被转换为pe的实际类型，也就是断言类型PathError
+		fmt.Printf("assert err is *os.PathError:%t\n",ok)
+		err = pe.Err
+	}
+	fmt.Printf("%s\n",err)
 }
--- src/main/Main.go
+++ src/main/Main.go
@@ -3,5 +3,9 @@ package main
 import "../chapter7"
 
 func main(){
-	chapter7.AssertTip()
+	chapter7.AssertTip(false)
+
+	chapter7.ShowErrorType()
+
+	chapter7.AssertWithError()
 }
