931f9d933c59b9c7c3a97f6b6b70129dea37b978
MichaelPaul0416 <18357903752m0@sina.cn>
2019-07-29 22:45:14 +0800

    go根据断言来确定类型实例-->java中的instanceOf关键字类似的效果

--- src/chapter7/Assert.go
+++ src/chapter7/Assert.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 	"fmt"
+	"bytes"
 )
 
 func AssertTip(c bool) {
@@ -26,12 +27,113 @@ func ShowErrorType() {
 
 //使用断言来返回错误
 func AssertWithError() {
-	_,err := os.Open("/no/such/file")
+	_, err := os.Open("/no/such/file")
 
-	if pe,ok := err.(*os.PathError);ok{
+	if pe, ok := err.(*os.PathError); ok {
 		//如果类型断言成功，那么err被转换为pe的实际类型，也就是断言类型PathError
-		fmt.Printf("assert err is *os.PathError:%t\n",ok)
+		fmt.Printf("assert err is *os.PathError:%t\n", ok)
 		err = pe.Err
 	}
-	fmt.Printf("%s\n",err)
+	fmt.Printf("%s\n", err)
+}
+
+//通过接口类型断言来查询特性--> java中的instanceOf 关键字
+type AbstractInvoker interface {
+	Invoke(s string) (i int, err error)
+}
+
+type HttpInvoker interface {
+	DoInvokerByHttp(code int, req string) (i int, err error)
+}
+
+type RpcInvoker interface {
+	DoInvokerByRpc(host, port, inter string) (i int, err error)
+}
+
+type Protocol struct {
+	Proto string
+	Code  int
+}
+
+type Restful Protocol
+
+type Dubbo Protocol
+
+func (r Restful) String() string {
+	var buf bytes.Buffer
+	buf.WriteString("Restful:{")
+	buf.WriteString("Protocol:" + r.Proto + ",")
+	buf.WriteString("Code:")
+	fmt.Fprintf(&buf, "%d}", r.Code)
+	return buf.String()
+}
+
+func (r Restful) Invoke(s string) (i int, err error) {
+	fmt.Printf("do invoke[%s]-->%s\n", s,r)
+	return 0, nil
+}
+
+func (r Restful) DoInvokerByHttp(code int, req string) (i int, err error) {
+	if r.Code >> 1 == 1{
+		fmt.Printf("restful protocol[code:%d,req:%s] --> %s\n",code,req,r)
+		return 0,nil
+	}
+
+	r.Invoke(req)
+	return 0,nil
+}
+
+func (d Dubbo) String() string {
+	var buf bytes.Buffer
+	buf.WriteString("Dubbo:{")
+	buf.WriteString("Protocol:" + d.Proto + ",")
+	buf.WriteString("Code:")
+	fmt.Fprintf(&buf, "%d}", d.Code)
+	return buf.String()
+}
+
+func (d Dubbo) Invoke(s string) (i int,err error){
+	fmt.Printf("do dubbo invoke[%s] --> %s\n",s,d)
+	return 0,nil
+}
+
+func (d Dubbo) DoInvokerByRpc(host,port,inter string)(i int,err error){
+	if d.Code >> 2 == 1{
+		fmt.Printf("dubbo protocol[to %s:%s--%s] --> %s\n",host,port,inter,d);
+		return 0,nil
+	}
+	d.Invoke("rpc service")
+	return 0,nil
+}
+
+func DynamicTypeByAssert(invoker AbstractInvoker){
+	if http,ok := invoker.(HttpInvoker);ok{
+		http.DoInvokerByHttp(200,"http request message")
+		return
+	}
+
+	if rpc,ok := invoker.(RpcInvoker);ok{
+		rpc.DoInvokerByRpc("192.168.56.1","6379","rpc request message")
+		return
+	}
+
+	invoker.Invoke("abstract invoke request")
+}
+
+func RealType(x interface{})string{
+	if x == nil{
+		return "NULL"
+	}else if _,ok := x.(int);ok{
+		return "int"
+	}else if _,ok := x.(uint);ok{
+		return "uint"
+	}else if b,ok := x.(bool);ok{
+		if b{
+			return "true"
+		}else {
+			return "false"
+		}
+	}else {
+		return "..."
+	}
 }
--- src/main/Main.go
+++ src/main/Main.go
@@ -1,11 +1,24 @@
 package main
 
-import "../chapter7"
+import (
+	"../chapter7"
+	"fmt"
+)
 
-func main(){
+func main() {
 	chapter7.AssertTip(false)
 
 	chapter7.ShowErrorType()
 
 	chapter7.AssertWithError()
+
+	//定义一个具体的子类Dubbo
+	dubbo := chapter7.Dubbo{Proto: "dubbo", Code: 1 << 2}
+	chapter7.DynamicTypeByAssert(dubbo)
+
+	http := chapter7.Restful{Proto:"restful",Code: 1 << 1}
+	chapter7.DynamicTypeByAssert(http)
+
+	//输出具体的类型
+	fmt.Printf("real type:%s\n",chapter7.RealType(true))
 }
