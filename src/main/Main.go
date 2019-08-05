621d051715f44172efac6910557a2621a76c0c54
MichaelPaul0416 <18357903752m0@sina.cn>
2019-08-05 23:00:45 +0800

    go中通道概念了解学习，同时单向通道和带缓冲区的通道了解学习

--- src/main/Main.go
+++ src/main/Main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-	"os"
 	"../chapter8"
+	"os"
+	"time"
+	"fmt"
 )
 
-func main(){
-	//chapter8.Tips()
+func main() {
+	if len(os.Args) == 1 {
+		//chapter8.Calculator(false)
+		m := make(chan int)
+		n := make(chan int)
+		go chapter8.Counter(m, 3)
+		go chapter8.ReceiveAndCalculator(n, m)
+		chapter8.FinalResult(n)
 
-	if os.Args[1] == "common"{
-		if os.Args[2] == "1" {
+		fmt.Printf("------------------------------------\n")
+		chapter8.ProducerAndConsumer(3,6)
+
+		time.Sleep(time.Hour * 1)
+
+	} else {
+		if os.Args[1] == "s" {
 			chapter8.StartTimeServer()
-		}else {
-			chapter8.ConnectServer()
-		}
-	}else if os.Args[1] == "special" {
-		if os.Args[2] == "1"{
-			chapter8.TimeServerWithSeveralResponse()
-		}else {
-			chapter8.ConnectServerWithSend()
+		} else if os.Args[1] == "c" {
+			chapter8.ClientToServer()
 		}
 	}
 
