cef55b3c4ba71808feff81ca7655d0da9999a462
MichaelPaul0416 <18357903752m0@sina.cn>
2019-08-06 22:23:13 +0800

    并发的goroutine处理，多个goroutine之间通讯，error处理，以及使用sync.WaitGroup计数器(java-->CountDownWatch)

--- src/main/Main.go
+++ src/main/Main.go
@@ -1,32 +1,9 @@
 package main
 
-import (
-	"../chapter8"
-	"os"
-	"time"
-	"fmt"
-)
+import "../chapter8"
 
-func main() {
-	if len(os.Args) == 1 {
-		//chapter8.Calculator(false)
-		m := make(chan int)
-		n := make(chan int)
-		go chapter8.Counter(m, 3)
-		go chapter8.ReceiveAndCalculator(n, m)
-		chapter8.FinalResult(n)
+func main(){
+	chapter8.WaitUtilAllDone(3,true)
 
-		fmt.Printf("------------------------------------\n")
-		chapter8.ProducerAndConsumer(3,6)
-
-		time.Sleep(time.Hour * 1)
-
-	} else {
-		if os.Args[1] == "s" {
-			chapter8.StartTimeServer()
-		} else if os.Args[1] == "c" {
-			chapter8.ClientToServer()
-		}
-	}
-
+	chapter8.CycleWhileUnConfirmTimes(10)
 }
