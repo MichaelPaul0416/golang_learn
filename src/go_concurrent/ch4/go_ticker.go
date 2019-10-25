53ee4fab032fa76c749f9a5d3a64ec472fffb90c
MichaelPaul0416 <18357903752m0@sina.cn>
2019-10-25 18:46:46 +0800

    断续器的使用demo，类似java中的定时任务(每隔一定时间执行)

---
+++ src/go_concurrent/ch4/go_ticker.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+/**
+声明一个断续器，每秒执行一次，通过timer.Ticker.C这个通道来接受，每次到期的通知
+ */
+func main() {
+	tc := time.NewTicker(1 * time.Second)
+	var i int
+	// for + range:这种形式是从一个可缓冲的通道中，持续不断的接受值，直到接受完毕或者通道被关闭
+	Loop:
+	for {
+		select {
+		case <-tc.C:
+			fmt.Printf("time:%v\n", time.Now())
+			if i < 10 {
+				i++
+			}else {
+				break Loop
+			}
+		}
+	}
+
+	fmt.Printf("done.\n")
+}
