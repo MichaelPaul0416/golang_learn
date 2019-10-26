f7526dc8337bb52b0a603c5422b4484f688e4163
MichaelPaul0416 <18357903752m0@sina.cn>
2019-10-26 11:25:32 +0800

    commit

--- src/go_concurrent/ch4/loadgen/lib/go_tickets.go
+++ src/go_concurrent/ch4/loadgen/lib/go_tickets.go
@@ -1,1 +1,81 @@
 package lib
+
+import (
+	"fmt"
+	"errors"
+)
+
+// goroutine的票据池.类似java的线程池
+
+// 定义票据池的接口
+type GoTickets interface {
+	Take()
+
+	Return()
+
+	Active() bool
+
+	Remainder() uint32
+
+	Total() uint32
+}
+
+// 接口实现类,封装在内部
+type myGoTickets struct {
+	total uint32
+	// 票据容器,其实就是一个带缓冲区的通道
+	ticketCh chan struct{}
+	active   bool
+}
+
+func NewGoTickets(total uint32) (GoTickets, error) {
+	gt := myGoTickets{}
+	if !gt.init(total){
+		errMsg := fmt.Sprintf("The goroutine ticket pool can not be initialized and total(%d)\n",total)
+		return nil,errors.New(errMsg)
+	}
+	return &gt,nil
+}
+
+
+func (gt *myGoTickets) init(total uint32)bool{
+	if gt.active{
+		return false
+	}
+
+	if total ==0 {
+		return false
+	}
+
+	ch := make(chan struct{},total)
+	n := int(total)
+
+	for i:=0;i<n;i++{
+		ch <- struct{}{}
+	}
+
+	gt.ticketCh = ch
+	gt.total = total
+	gt.active = true
+	return true
+}
+
+func (gt *myGoTickets) Take(){
+	<- gt.ticketCh
+}
+
+func (gt *myGoTickets) Return(){
+	gt.ticketCh <- struct{}{}
+}
+
+func (gt * myGoTickets) Active() bool{
+	return gt.active
+}
+
+func (gt *myGoTickets) Total() uint32{
+	return gt.total
+}
+
+func (gt *myGoTickets) Remainder() uint32 {
+	return uint32(len(gt.ticketCh))
+}
--- src/go_concurrent/ch4/loadgen/lib/go_tickets_test.go
+++ src/go_concurrent/ch4/loadgen/lib/go_tickets_test.go
@@ -1,1 +1,36 @@
 package lib
+
+import (
+	"testing"
+	"fmt"
+	"time"
+	"sync"
+)
+
+func TestMyGoTickets_Take(t *testing.T) {
+	gt, err := NewGoTickets(2)
+	fmt.Printf("total:%d\n", gt.Total())
+	if err != nil {
+		fmt.Printf("init go tickets error:%v\n", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			defer func() {
+				fmt.Printf("before return left:%d\n", gt.Remainder())
+				gt.Return()
+				fmt.Printf("after return left:%d\n", gt.Remainder())
+				wg.Done()
+			}()
+			gt.Take()
+			fmt.Printf("current cycle:%d\n", i)
+			time.Sleep(1000 * time.Millisecond)
+		}(i)
+	}
+
+	wg.Wait()
+
+}
